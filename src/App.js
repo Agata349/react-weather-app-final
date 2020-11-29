1966964f4565df2c4bca8d9eac5bec7dc8a8f117
Agata <agata.piekarczyk90@gmail.com>
2020-11-29 17:17:55 +0100

    rel="noreferrer" issue fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Agata Piekarczyk and is {""}
-      <a href="https://github.com/Agata349/react-weather-app-final" target="_blank">
+      <a href="https://github.com/Agata349/react-weather-app-final" target="_blank" rel="noreferrer">
         open-sourced on GitHub
       </a>
       </footer>
