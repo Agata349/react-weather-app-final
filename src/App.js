db03dc9ccad96f377506ead33875ec779620ed0c
Agata <agata.piekarczyk90@gmail.com>
2020-11-29 17:44:06 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
-      <footer>
-        This project was coded by Agata Piekarczyk and is {""}
-      <a href="https://github.com/Agata349/react-weather-app-final" target="_blank" rel="noreferrer">
-        open-sourced on GitHub
-      </a>
-      </footer>
+        <footer>
+          This project was coded by Agata Piekarczyk and is {""}
+        <a href="https://github.com/Agata349/react-weather-app-final" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
+        </a>
+        </footer>
       </div>
+    </div> 
   );
 }
 
