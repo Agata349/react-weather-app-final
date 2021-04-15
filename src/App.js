644892a1846ff4cab4fccab6d68ba148d2fb35e9
Agata <agata.piekarczyk90@gmail.com>
2021-04-15 20:56:32 +0200

    Add links

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
       <Weather defaultCity="New York"/>
         <footer>
           This project was coded by Agata Piekarczyk and is {""}
-        <a href="/" target="_blank" rel="noreferrer">
-          open-sourced on GitHub
+        <a href="https://github.com/Agata349/react-weather-app-final" target="_blank" rel="noreferrer">
+          open-sourced on GitHub and hosted on <a href="https://bit.ly/33P0MaZ" target="_blank" rel="noreferrer">Netlify</a>
         </a>
         </footer>
       </div>
