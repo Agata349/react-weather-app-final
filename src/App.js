2e90fbec173b0e3455e31034b2d39d3de1414167
Agata <agata.piekarczyk90@gmail.com>
2021-05-24 00:35:13 +0200

    link changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <footer>
           This project was coded by Agata Piekarczyk and is {""}
         <a href="https://github.com/Agata349/react-weather-app-final" target="_blank" rel="noreferrer">
-          open-sourced on GitHub</a> and hosted on <a href="https://bit.ly/3bKlDjy" target="_blank" rel="noreferrer"> Netlify </a>
+          open-sourced on GitHub</a> and hosted on <a href="https://bit.ly/3vgp35s" target="_blank" rel="noreferrer"> Netlify </a>
         </footer>
       </div>
     </div> 
